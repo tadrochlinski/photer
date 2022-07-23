4d9114387a3a8845a73748b1d595e14af543c010
tadrochlinski <tadrochlinski@gmail.com>
2022-07-23 22:33:07 +0200

    handle submit done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import GlobalStyles from "./Components/GlobalStyles/GlobalStyles";
 import AppWrapper from "./Components/AppWrapper/AppWrapper";
 import Navbar from "./Components/Navbar/Navbar";
+import { useRef, useState } from "react";
 
 const App = () =>{
+  const inputRef = useRef(null);
+  const [query, setQuery] = useState('');
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    const inputValue = inputRef.current.value;
+    setQuery(inputValue);
+  }
+
   return (
     <>
       <GlobalStyles/>
       <AppWrapper>
         <Navbar>
           <h1>Photer 📷</h1>
-          <form>
+          <form onSubmit = {handleSubmit}>
             <input 
             type="text" 
             placeholder="e.g. apple"
+            ref={inputRef}
             />
             <button type="submit">
               search
