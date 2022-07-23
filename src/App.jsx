ec4371fef5eaa49573bbcc8742824fbcb7d43df7
tadrochlinski <tadrochlinski@gmail.com>
2022-07-23 23:12:35 +0200

    fetching photos and showing alts in list done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 import GlobalStyles from "./Components/GlobalStyles/GlobalStyles";
 import AppWrapper from "./Components/AppWrapper/AppWrapper";
 import Navbar from "./Components/Navbar/Navbar";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect} from "react";
 
 const App = () =>{
   const inputRef = useRef(null);
   const [query, setQuery] = useState('');
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() =>{
+    const fetchPhotos = async () =>{
+      const url = `https://api.unsplash.com/search/photos/?query=${query}&client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data.results);
+      setPhotos(data.results);
+    }
+
+    if(query) fetchPhotos();
+  }, [query]);
 
   const handleSubmit = (e) =>{
     e.preventDefault();
     const inputValue = inputRef.current.value;
-    setQuery(inputValue);
+    if(inputValue) setQuery(inputValue);
   }
 
   return (
@@ -31,6 +44,9 @@ const App = () =>{
           </form>
         </Navbar>
 
+        <ul>
+          {photos.map(photo => (<li key={photo.alt_description}>{photo.alt_description}</li>))}
+        </ul>
       </AppWrapper>
     </>
   );
